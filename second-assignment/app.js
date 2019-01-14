421c5e15b0ab7ff09ff438b5d1889712a65904d4
Logan Mace <lmmace92@hotmail.com>
2019-01-14 16:10:31 -0600

    server built

--- second-assignment/app.js
+++ second-assignment/app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.use((req, res, next) => {
+  console.log('Always hits');
+  next();
+});
 
+app.use('/users', (req, res, next) => {
+  res.send('<h1>Users</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Home</h1>');
+});
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
