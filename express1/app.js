44c531d6f83b051406e97a64b26af1db6fc59cc2
Logan Mace <lmmace92@hotmail.com>
2019-01-14 15:55:08 -0600

    full example

--- express1/app.js
+++ express1/app.js
@@ -1,18 +1,29 @@
-const http = require('http');
 const express = require('express');
 const app = express();
 const port = 3001;
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
+// app.use((req, res, next) => {
+//   console.log('In the middleware');
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log('In the OTHER middleware');
+//   res.send('<h1>Hello from Express!</h1>');
+// });
+app.use('/', (req, res, next) => {
+  console.log('This always runs!');
   next();
 });
 
-app.use((req, res, next) => {
-  console.log('In the OTHER middleware');
-  res.sendStatus(200);
-});
+app.use('/add-product', (req, res, next) => {
+    console.log('In the add product middleware');
+    res.send('<h1>Add Product!</h1>');
+  });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+    console.log('In the middleware');
+    res.send('<h1>Hello from Express!</h1>');
+  });
 
-server.listen(port, () => console.log(`Listening on port: ${port}`));
+app.listen(port, () => console.log(`Listening on port: ${port}`));
