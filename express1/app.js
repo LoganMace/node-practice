2b925db7a45dd636af974b1fc5ce93876e8461da
Logan Mace <lmmace92@hotmail.com>
2019-01-15 10:01:17 -0600

    added routes

--- express1/app.js
+++ express1/app.js
@@ -1,29 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
 
-// app.use((req, res, next) => {
-//   console.log('In the middleware');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+// app.use('/', (req, res, next) => {
+//   console.log('This always runs!');
 //   next();
 // });
 
-// app.use((req, res, next) => {
-//   console.log('In the OTHER middleware');
-//   res.send('<h1>Hello from Express!</h1>');
-// });
-app.use('/', (req, res, next) => {
-  console.log('This always runs!');
-  next();
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page Not Found</h1>');
 });
-
-app.use('/add-product', (req, res, next) => {
-    console.log('In the add product middleware');
-    res.send('<h1>Add Product!</h1>');
-  });
-
-app.use('/', (req, res, next) => {
-    console.log('In the middleware');
-    res.send('<h1>Hello from Express!</h1>');
-  });
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
