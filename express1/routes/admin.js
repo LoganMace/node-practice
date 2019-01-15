ce0471ca22de8df679eb077e8f17c75bf9db59e4
Logan Mace <lmmace92@hotmail.com>
2019-01-15 17:11:50 -0600

    added pug

--- express1/routes/admin.js
+++ express1/routes/admin.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-
 const rootDir = require('../util/path');
+
+const products = [];
 
 router.get('/add-product', (req, res, next) => {
   // console.log('In the add product middleware');
@@ -10,8 +11,11 @@ router.get('/add-product', (req, res, next) => {
 });
 
 router.post('/add-product', (req, res, next) => {
-console.log(req.body);
+// console.log(req.body);
+products.push({title: req.body.title});
+
 res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- express1/routes/shop.js
+++ express1/routes/shop.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
   // console.log('In the middleware');
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = router;
